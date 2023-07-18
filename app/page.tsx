7ed24876b5d9d85b9f57255944af94036db23eff
unknown <dnaryshev@chapman.edu>
2023-07-18 11:18:09 -0700

    mobile height fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const navigation = [
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+		<div className="flex flex-col items-center justify-center w-screen min-h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
 					{navigation.map((item) => (
