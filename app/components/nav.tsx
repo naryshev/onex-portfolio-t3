9d8465b5f2ce74092d78e3457589321c44a1e08a
unknown <dnaryshev@chapman.edu>
2023-07-18 18:53:58 -0700

    nav order

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -35,16 +35,16 @@ export const Navigation: React.FC = () => {
 							Projects
 						</Link>
 						<Link
-							href="/contact"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
-						>
-							Contact
-						</Link>
-						<Link
 							href="/booking"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							Booking
+						</Link>
+						<Link
+							href="/contact"
+							className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+							Contact
 						</Link>
 					</div>
 					<div className="flex justify-between items-center space-x-4">
