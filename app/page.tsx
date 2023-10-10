3588c246f7808511d9eb91e0d06558194aa01b35
unknown <dnaryshev@chapman.edu>
2023-10-09 17:12:05 -0700

    project update

--- app/page.tsx
+++ app/page.tsx
@@ -50,9 +50,9 @@ export default function Home() {
 				<div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
 					
 
-				<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+				<hr className="my-6 sm:mx-auto lg:my-8" />
 				<div className="sm:flex sm:items-center sm:justify-between">
-					<span className="text-sm text-zinc-500 sm:text-center dark:text-zinc-400">© 2023 <a href="https://onex.zip/" className="hover:underline">ONEX Records™</a>. All Rights Reserved.
+					<span className="text-sm text-zinc-500 sm:text-center">© 2023 <a href="https://onex.zip/" className="hover:underline">ONEX Records™</a>. All Rights Reserved.
 					</span>
 					
 				</div>
