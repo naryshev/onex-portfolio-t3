7074a95fd7004f2eea207756dbf548fc37722aef
unknown <dnaryshev@chapman.edu>
2024-02-15 13:07:14 -0800

    home page improvements

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {Footer} from "./components/footer";
 import {motion} from "framer-motion"
 
 const navigation = [
-	{ name: "Credits", href: "/credits" },
+	// { name: "Credits", href: "/credits" },
 	// { name: "Store", href: "/store" },
 	{ name: "Booking", href: "/booking" },
 	// { name: "About", href: "/about" },
@@ -18,7 +18,7 @@ export default function Home() {
 		<div className="flex flex-col items-center justify-center w-screen min-h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
-					{navigation.map((item) => (
+					{/* {navigation.map((item) => (
 						<Link
 							key={item.href}
 							href={item.href}
@@ -26,7 +26,7 @@ export default function Home() {
 						>
 							{item.name}
 						</Link>
-					))}
+					))} */}
 				</ul>
 			</nav>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
