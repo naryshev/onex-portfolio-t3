bc4b5b3fd26ad7bbf2df5e9b41c154237e53e505
unknown <dnaryshev@chapman.edu>
2024-02-27 18:31:00 -0800

    newsletter form rounding

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -63,13 +63,13 @@ export default function Example() {
                       <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                           <svg className="w-5 h-5 text-slate-50 dark:text-slate-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                       </div>
-                      <input className="animate-pulse block p-3 pl-10 w-full text-sm text-slate-950 bg-transparent border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-900 dark:bg-transparent-700 dark:border-gray-300 dark:placeholder-gray-100 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" onChange={(e) => setMail(e.target.value)} value={mail} />
+                      <input className="animate-pulse block p-3 pl-10 w-full text-sm text-slate-950 bg-transparent border border-gray-300 rounded focus:ring-primary-500 focus:border-primary-900 dark:bg-transparent-700 dark:border-gray-300 dark:placeholder-gray-100 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" onChange={(e) => setMail(e.target.value)} value={mail} />
                   </div>
                   <div>
                       {/* <button type="submit" className="hover:animate-pulse py-3 px-5 w-full text-sm font-medium text-center text-slate-50 border rounded-l-lg cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button> */}
                       <button
                         type="submit"
-                        className="hover:animate-pulse py-3 px-5 w-full text-sm font-medium text-center text-slate-50 border rounded-l-lg cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        className="hover:animate-pulse py-3 px-5 w-full text-sm font-medium text-center text-slate-50 border rounded-lg cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     >
                         {loading ? (
                             <div
